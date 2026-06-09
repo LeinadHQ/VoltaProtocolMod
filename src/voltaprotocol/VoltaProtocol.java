ccdf407ce5e30f45ec6c27f20082402646540581
LeinadHQ <daniellolcocycr@gmail.com>
2026-06-09 17:41:49 -0400

    Update VoltaProtocol.java

--- src/voltaprotocol/VoltaProtocol.java
+++ src/voltaprotocol/VoltaProtocol.java
@@ -3,18 +3,24 @@ package voltaprotocol;
 import arc.util.Log;
 import mindustry.mod.Mod;
 
+import voltaprotocol.content.VPLiquids;
 import voltaprotocol.content.VPBlocks;
 import voltaprotocol.content.VPItems;
 import voltaprotocol.content.VPTechTree;
+import voltaprotocol.content.VPPlanets;
+import voltaprotocol.content.VPSectors;
 
 public class VoltaProtocol extends Mod {
 
-    @Override
+@Override
     public void loadContent(){
         Log.info("[Volta Protocol] Iniciando carga de contenido síncrono...");
 
         VPItems.load();
+        VPLiquids.load();
         VPBlocks.load();
+        VPPlanets.load();
+        VPSectors.load();
         VPTechTree.load();
 
         Log.info("[Volta Protocol] Carga completada de forma segura.");
