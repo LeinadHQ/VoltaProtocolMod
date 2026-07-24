abde45d970d8448e0d8c7ba15aa431780ab12aaa
LeinadHQ <daniellolcocycr@gmail.com>
2026-07-24 00:55:25 -0400

    Update VPTechTree.java

--- src/voltaprotocol/content/VPTechTree.java
+++ src/voltaprotocol/content/VPTechTree.java
@@ -12,9 +12,9 @@ public class VPTechTree {
     public static void load() {
         Log.info("[Volta Protocol] Acoplando ramas de investigación");
 
-        VPBlocks.silverWall.researchCost      = ItemStack.with(VPItems.silver, 150);
-        VPBlocks.silverWallLarge.researchCost  = ItemStack.with(VPItems.silver, 350);
-        VPBlocks.palladiumWall.researchCost    = ItemStack.with(VPItems.palladium, 200);
+        VPBlocks.silverWall.researchCost = ItemStack.with(VPItems.silver, 150);
+        VPBlocks.silverWallLarge.researchCost = ItemStack.with(VPItems.silver, 350);
+        VPBlocks.palladiumWall.researchCost = ItemStack.with(VPItems.palladium, 200);
         VPBlocks.palladiumWallLarge.researchCost = ItemStack.with(VPItems.palladium, 450);
         VPBlocks.regenerativeWall.researchCost = ItemStack.with(VPItems.bioComposite, 250, VPItems.palladium, 100);
         VPBlocks.regenerativeWallLarge.researchCost = ItemStack.with(VPItems.bioComposite, 600, VPItems.palladium, 300);
@@ -25,6 +25,10 @@ public class VPTechTree {
             ItemStack.with(VPItems.silver, 600, VPItems.palladium, 400);
         VPBlocks.liquidCargoUnloadPoint.researchCost =
             ItemStack.with(VPItems.silver, 225, VPItems.palladium, 100);
+
+        VPBlocks.palladiumLiquidBridge.researchCost = ItemStack.with(VPItems.palladium, 80, Items.metaglass, 80);
+        VPBlocks.palladiumLiquidOverflow.researchCost = ItemStack.with(VPItems.palladium, 50, Items.metaglass, 50);
+        VPBlocks.palladiumLiquidUnderflow.researchCost = ItemStack.with(VPItems.palladium, 50, Items.metaglass, 50);
 
         VPBlocks.moduleSilver.researchCost =
             ItemStack.with(VPItems.silver, 2000, VPItems.palladium, 500);
@@ -79,7 +83,6 @@ public class VPTechTree {
                     }
                 });
             }
-
             // Muros
             if (VPBlocks.silverWall != null) {
                 new TechNode(voltaNodeRef, VPBlocks.silverWall, VPBlocks.silverWall.researchRequirements());
@@ -105,7 +108,6 @@ public class VPTechTree {
                     }
                 }
             }
-
             // Módulos
             if (VPBlocks.moduleBasic != null) {
                 new TechNode(voltaNodeRef, VPBlocks.moduleBasic, VPBlocks.moduleBasic.researchRequirements());
@@ -131,28 +133,45 @@ public class VPTechTree {
                     }
                 }
             }
-
             // Liquid-logistic
             if (VPBlocks.palladiumConduit != null) {
                 new TechNode(voltaNodeRef, VPBlocks.palladiumConduit, VPBlocks.palladiumConduit.researchRequirements());
                 TechNode palCondNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumConduit);
-                if (palCondNode != null && VPBlocks.palladiumLiquidRouter != null) {
-                    new TechNode(palCondNode, VPBlocks.palladiumLiquidRouter, VPBlocks.palladiumLiquidRouter.researchRequirements());
-                    TechNode palRouterNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidRouter);
-                    if (palRouterNode != null) {
-                        if (VPBlocks.palladiumLiquidContainer != null) {
-                            new TechNode(palRouterNode, VPBlocks.palladiumLiquidContainer, VPBlocks.palladiumLiquidContainer.researchRequirements());
-                            TechNode palContNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidContainer);
-                            if (palContNode != null && VPBlocks.liquidCargoLoader != null) {
-                                new TechNode(palContNode, VPBlocks.liquidCargoLoader, VPBlocks.liquidCargoLoader.researchRequirements());
-                                TechNode cargoLoaderNode = TechTree.all.find(t -> t.content == VPBlocks.liquidCargoLoader);
-                                if (cargoLoaderNode != null && VPBlocks.liquidCargoUnloadPoint != null) {
-                                    new TechNode(cargoLoaderNode, VPBlocks.liquidCargoUnloadPoint, VPBlocks.liquidCargoUnloadPoint.researchRequirements());
+                
+                if (palCondNode != null && VPBlocks.palladiumLiquidBridge != null) {
+                    new TechNode(palCondNode, VPBlocks.palladiumLiquidBridge, VPBlocks.palladiumLiquidBridge.researchRequirements());
+                    TechNode palBridgeNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidBridge);
+                    
+                    if (palBridgeNode != null && VPBlocks.palladiumLiquidRouter != null) {
+                        new TechNode(palBridgeNode, VPBlocks.palladiumLiquidRouter, VPBlocks.palladiumLiquidRouter.researchRequirements());
+                        TechNode palRouterNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidRouter);
+                        
+                        if (palRouterNode != null) {
+                            if (VPBlocks.palladiumLiquidContainer != null) {
+                                new TechNode(palRouterNode, VPBlocks.palladiumLiquidContainer, VPBlocks.palladiumLiquidContainer.researchRequirements());
+                                TechNode palContNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidContainer);
+                                if (palContNode != null && VPBlocks.liquidCargoLoader != null) {
+                                    new TechNode(palContNode, VPBlocks.liquidCargoLoader, VPBlocks.liquidCargoLoader.researchRequirements());
+                                    TechNode cargoLoaderNode = TechTree.all.find(t -> t.content == VPBlocks.liquidCargoLoader);
+                                    if (cargoLoaderNode != null && VPBlocks.liquidCargoUnloadPoint != null) {
+                                        new TechNode(cargoLoaderNode, VPBlocks.liquidCargoUnloadPoint, VPBlocks.liquidCargoUnloadPoint.researchRequirements());
+                                    }
                                 }
                             }
-                        }
-                        if (VPBlocks.palladiumLiquidJunction != null) {
-                            new TechNode(palRouterNode, VPBlocks.palladiumLiquidJunction, VPBlocks.palladiumLiquidJunction.researchRequirements());
+                            
+                            if (VPBlocks.palladiumLiquidJunction != null) {
+                                new TechNode(palRouterNode, VPBlocks.palladiumLiquidJunction, VPBlocks.palladiumLiquidJunction.researchRequirements());
+                                TechNode palJunctionNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidJunction);
+                                
+                                if (palJunctionNode != null && VPBlocks.palladiumLiquidOverflow != null) {
+                                    new TechNode(palJunctionNode, VPBlocks.palladiumLiquidOverflow, VPBlocks.palladiumLiquidOverflow.researchRequirements());
+                                    TechNode palOverflowNode = TechTree.all.find(t -> t.content == VPBlocks.palladiumLiquidOverflow);
+                                    
+                                    if (palOverflowNode != null && VPBlocks.palladiumLiquidUnderflow != null) {
+                                        new TechNode(palOverflowNode, VPBlocks.palladiumLiquidUnderflow, VPBlocks.palladiumLiquidUnderflow.researchRequirements());
+                                    }
+                                }
+                            }
                         }
                     }
                 }
